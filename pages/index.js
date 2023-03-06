7375a07bcc880cfb9576b8ca5cfdb392d351e333
Sajjad <sajjadjoon72@yahoo.com>
2023-03-06 14:14:35 +0330

    fix some bugs

--- pages/index.js
+++ pages/index.js
@@ -1,110 +1,125 @@
 import Loading from "@/components/Loading";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import React from "react";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = () => {
-    const fetchDoctors = async ({ pageParam = 0 }) => {
+    const [modal, setModal] = useState(false);
+    const [thisDoctor, setThisDoctor] = useState(null);
+    const router = useRouter();
+    const fetchDoctors = async ({ pageParam = 1 }) => {
         const res = await fetch(
-            "https://www.tebinja.com/api/v1/doctors/searchi?page=" + pageParam
+            "https://rickandmortyapi.com/api/character/?page=" + pageParam
         );
         return res.json();
     };
 
-    const { data, error, fetchNextPage, hasNextPage, status } =
-        useInfiniteQuery({
-            queryKey: ["doctors"],
-            queryFn: fetchDoctors,
-            getNextPageParam: (lastPage, pages) => {
-                if (lastPage.success) {
-                    return pages.length + 1;
-                }
-            },
-        });
+    const { data, fetchNextPage, hasNextPage, status } = useInfiniteQuery({
+        queryKey: ["doctors"],
+        queryFn: fetchDoctors,
+        getNextPageParam: (lastPage, pages) => {
+            if (lastPage.info.next) {
+                return pages.length + 1;
+            }
+        },
+    });
 
     if (status === "loading") return <Loading />;
 
-    console.log(data.pages.length);
+    const fetchOneDoctor = async (id) => {
+        const res = await fetch(
+            "https://rickandmortyapi.com/api/character/" + id
+        );
+        return res.json();
+    };
+
+    const showThis = async (id) => {
+        console.log(id);
+        setModal(true);
+        router.push({ hash: `${id}` }, undefined, { shallow: true });
+        const thisDoctor = await fetchOneDoctor(id);
+        setThisDoctor(thisDoctor);
+    };
+
+    const closeModal = () => {
+        setModal(false);
+        router.back();
+        setThisDoctor(null);
+    };
 
     return (
-        <div className="container">
-            <div className="row">
-                {status === "success" && (
-                    <InfiniteScroll
-                        dataLength={data?.pages.length}
-                        next={fetchNextPage}
-                        hasMore={hasNextPage}
-                        loader={<Loading />}
-                    >
-                        {data.pages.map((group, i) => (
-                            <React.Fragment key={i}>
-                                {group.doctors.hits.map((project, i) => (
-                                    <div
-                                        key={i}
-                                        className="col-12 col-sm-12 col-md-6 my-3"
-                                    >
-                                        <div className="card border-0 shadow text-center">
-                                            <div className="card-body">
-                                                <div className="card-avatar">
-                                                    <img
-                                                        src={`https://www.tebinja.com/img/uploads/doctors/thumbnails/${project._source.url}`}
-                                                        alt=""
-                                                    />
+        <>
+            {modal && (
+                <div
+                    style={{
+                        position: "fixed",
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "black",
+                        opacity: ".5",
+                        zIndex: "10",
+                    }}
+                    onClick={closeModal}
+                ></div>
+            )}
+            {modal && (
+                <div
+                    style={{
+                        position: "fixed",
+                        top: "50px",
+                        height: "20rem",
+                        width: "30rem",
+                        backgroundColor: "yellow",
+                        opacity: ".9",
+                        zIndex: "20",
+                    }}
+                >
+                    {!thisDoctor ? "Loading..." : <h3>{thisDoctor.name}</h3>}
+                </div>
+            )}
+            <div className="container">
+                <div className="row">
+                    {status === "success" && (
+                        <InfiniteScroll
+                            dataLength={data?.pages.length}
+                            next={fetchNextPage}
+                            hasMore={hasNextPage}
+                            loader={<Loading />}
+                        >
+                            {data.pages.map((group, i) => (
+                                <React.Fragment key={i}>
+                                    {group.results.map((project, i) => (
+                                        <div
+                                            key={i}
+                                            className="col-12 col-sm-12 col-md-6 my-3"
+                                            onClick={() => showThis(project.id)}
+                                        >
+                                            <div className="card border-0 shadow text-center">
+                                                <div className="card-body">
+                                                    <div className="card-avatar">
+                                                        <img
+                                                            src={`${project.image}`}
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                    <h5 className="card-title text-dark">
+                                                        {project.name +
+                                                            " " +
+                                                            project.species}
+                                                    </h5>
                                                 </div>
-                                                <h5 className="card-title text-dark">
-                                                    {project._source.fname +
-                                                        " " +
-                                                        project._source.lname}
-                                                </h5>
                                             </div>
                                         </div>
-                                    </div>
-                                ))}
-                            </React.Fragment>
-                        ))}
-                    </InfiniteScroll>
-                )}
+                                    ))}
+                                </React.Fragment>
+                            ))}
+                        </InfiniteScroll>
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     );
-
-    // return (
-    //     <div className="container">
-    //         <div className="row">
-    //             {users.doctors.hits.map((item, i) => (
-    //                 <div key={i} className="col-12 col-sm-12 col-md-6 my-3">
-    //                     <div className="card border-0 shadow text-center">
-    //                         <div
-    //                             onClick={() => {
-    //                                 setModal(true);
-    //                                 setModalData(item);
-    //                             }}
-    //                             className="card-body"
-    //                         >
-    //                             <div className="card-avatar">
-    //                                 <img
-    //                                     src={`https://www.tebinja.com/img/uploads/doctors/thumbnails/${item._source.url}`}
-    //                                     onError={(e) => {
-    //                                         e.currentTarget.src = icon;
-    //                                     }}
-    //                                     alt=""
-    //                                 />
-    //                             </div>
-    //                             <h5 className="card-title text-dark">
-    //                                 {item._source.fname +
-    //                                     " " +
-    //                                     item._source.lname}
-    //                             </h5>
-    //                             <button className="btn btn-sm btn-success badge">
-    //                                 @{item._id}
-    //                             </button>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     </div>
-    // );
 };
 
 export default Home;
